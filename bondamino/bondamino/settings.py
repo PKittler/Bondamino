eff44ae662ee278a0ae92a1b1ee723473837782d
PGensel <gensel@burando.de>
2025-04-08 14:03:08 +0200

    fixed the problem, that the iframe will not be shown in the browser (sameorigin problem)

--- bondamino/bondamino/settings.py
+++ bondamino/bondamino/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # PDB files will be here stored temporarily
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
